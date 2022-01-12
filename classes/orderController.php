1ff270f04a60ac51a37cf961b128bc4214faab73
tesco <zs0lt.santa@gmail.com>
2022-01-12 14:16:59 +0100

    process the data

--- classes/orderController.php
+++ classes/orderController.php
@@ -31,4 +31,30 @@ class orderController
             break;
         }
     }
+
+    /**
+     * sort the order by priority and creation date
+     */
+    public function sortOrderData()
+    {
+        usort($this->orderDataAll, function ($a, $b) {
+            $pc = -1 * ($a['priority'] <=> $b['priority']);
+            return $pc == 0 ? $a['created_at'] <=> $b['created_at'] : $pc;
+        });
+    }
+
+    /**
+     * filter the list of the orders
+     * 
+     * @stock: json object, product id with minimum quantity
+     */
+    public function filterOrderDataByStock($stock)
+    {
+        foreach($this->orderDataAll as $index => $item) {
+            if ($stock->{$item['product_id']} < $item['quantity']) {
+                unset($this->orderDataAll[$index]);
+            }
+        }
+        $this->orderDataAll = array_values($this->orderDataAll);
+    }
 }
--- get_fulfillable_orders.php
+++ get_fulfillable_orders.php
@@ -8,3 +8,15 @@ if ($argc != 2) {
 require_once __DIR__ . '/class/orderController.php';
 
 $orderController = new orderController();
+
+//check for input data
+$stockInput = json_decode($argv[1]);
+if(empty($stockInput)) {
+    echo 'Invalid json!';
+    exit(1);
+}
+
+//filter the array
+$orderController->filterOrderDataByStock($stockInput);
+//sort by priority and date
+$orderController->sortOrderData();
