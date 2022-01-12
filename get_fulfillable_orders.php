72d146a674e0d77116c85052fa99ef4757d5fe32
tesco <zs0lt.santa@gmail.com>
2022-01-12 14:14:04 +0100

    init order class

--- get_fulfillable_orders.php
+++ get_fulfillable_orders.php
@@ -1,2 +1,10 @@
 <?php
 
+if ($argc != 2) {
+    echo 'Ambiguous number of parameters!';
+    exit(1);
+}
+
+require_once __DIR__ . '/class/orderController.php';
+
+$orderController = new orderController();
