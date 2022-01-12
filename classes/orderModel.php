7289055191e42a68558bbdaf3ba516c0ea5edc03
tesco <zs0lt.santa@gmail.com>
2022-01-12 14:11:37 +0100

    read csv file

--- classes/orderModel.php
+++ classes/orderModel.php
@@ -1,1 +1,32 @@
 <?php
+
+class orderModel
+{
+    private $ordersData;
+
+    function __construct()
+    {
+        $row = 1;
+        $ordersH = [];
+        if (($handle = fopen(__DIR__ . '/../orders.csv', 'r')) !== false) {
+            while (($data = fgetcsv($handle)) !== false) {
+                if ($row == 1) {
+                    $ordersH = $data;
+                } else {
+                    $o = [];
+                    for ($i = 0; $i < count($ordersH); $i++) {
+                        $o[$ordersH[$i]] = $data[$i];
+                    }
+                    $this->ordersData[] = $o;
+                }
+                $row++;
+            }
+            fclose($handle);
+        }
+    }
+
+    function getAll()
+    {
+        return $this->ordersData;
+    }
+}
